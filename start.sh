ca268c9999db09d3644262712a3c4a8e73532084
MykhailoKononov <kononomisha@gmail.com>
2025-08-01 14:54:44 +0200

    dockerization

---
+++ start.sh
@@ -0,0 +1,19 @@
+echo "⏳ Waiting for PostgreSQL..."
+until pg_isready -h db -p 5432 -U "$DB_USER"; do
+  sleep 1
+done
+echo "✅ PostgreSQL is ready"
+
+python manage.py migrate --no-input
+
+python manage.py shell << EOF
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="${DJANGO_SUPERUSER_USERNAME}").exists():
+    User.objects.create_superuser(
+        username="${DJANGO_SUPERUSER_USERNAME}",
+        password="${DJANGO_SUPERUSER_PASSWORD}"
+    )
+EOF
+
+python manage.py runserver 0.0.0.0:8000
